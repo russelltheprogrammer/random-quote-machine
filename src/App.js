11aa4fc4646dd9b3c23ea63f554e0361c890bc7b
russelltheprogrammer <russelltheprogrammer@gmail.com>
2021-06-01 16:53:59 -0400

    import API

--- src/App.js
+++ src/App.js
@@ -1,65 +1,56 @@
 import './App.css';
 import React from 'react';
 
-
 class App extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-      quote: "",
-      author: ""
-      };
-      this.handleClick = this.handleClick.bind(this);
+constructor(props){
+  super(props);
+    this.state = {
+      loading: true,
+      quote: ""
+  }
+  this.handleClick = this.handleClick.bind(this);
+}
+  
+
+  async componentDidMount() {
+    const url = 'https://type.fit/api/quotes';
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    let randomIndex = Math.floor(Math.random() * jsonData.length);
+
+    this.setState({quote: Object.values(jsonData[randomIndex]), loading: false })
+    console.log(jsonData[randomIndex]);
   }
 
-/*
-function FetchAPI() {
-
-  const apiGet = () => {
-    fetch('https://type.fit/api/quotes')
-    .then((response) => response.json())
-    .then((json) => console.log(json);)
+  handleClick(){
+    this.setState({
+      quote: "new quote"
+    });
   };
-}
-*/
 
-    async componentDidMount(){
-    const response = await fetch('https://type.fit/api/quotes');
-    const jsondata = await response.json();
-
-    let randomIndex = Math.floor(Math.random() * jsondata.length)
-    
-    let quoteAndAuthorArray = Object.values(jsondata[randomIndex]);
-
-    this.setState({ quote: quoteAndAuthorArray[0] , author: quoteAndAuthorArray[1] });
+  render() {
+if (this.state.loading) {
+  return <div>loading...</div>;
 }
 
-    
-
-handleClick(){
-  this.setState({
-    quote: "new quote",
-    author: "new author"
-  });
-};
+if (!this.state.quote) {
+  return <div>No Quote!</div>;
+}
 
 
-render () {
-  
-  return (
-      
-    <div>
+    return (
+<div>
       <wrapper className="centered" id="quote-box"> {/* wrapper is only used because FCC project asks for it */}
 
           <div className="row">
           <div className="col">
               
-      <div id="text">"{this.state.quote}"</div>
+      <div id="text">"{this.state.quote[0]}"</div>
       </div>
       </div>
       <div className="row">
       <div className="col">
-      <div id="author">~{this.state.author}</div>
+      <div id="author">~{this.state.quote[1]}</div>
       </div>
       </div>
 
@@ -79,34 +70,12 @@ render () {
       </div>
    </wrapper>
    </div>
-      
-   
-  );
+    );
   }
-};
+}
+
 
 export default App;
 
 
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 
